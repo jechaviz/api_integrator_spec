ad66d6e4bd28e67ad3883eb70ad725f56cbb6525
jesus <461913@petco.com>
2024-10-02 13:37:46 -0600

    refactor: Rename `data` attribute to `_data` in YamlObject class

--- api_integrator_spec/domain/value_objects/yaml_object.py
+++ api_integrator_spec/domain/value_objects/yaml_object.py
@@ -1,13 +1,13 @@
 class YamlObject:
     def __init__(self, data):
-        self.data = data
+        self._data = data
 
     def to_dict(self):
-        return self.data
+        return self._data
 
     def __getattr__(self, key):
-        if key in self.data:
-            value = self.data[key]
+        if key in self._data:
+            value = self._data[key]
             if isinstance(value, dict):
                 return YamlObject(value)
             elif isinstance(value, list):
@@ -18,14 +18,14 @@ class YamlObject:
             raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{key}'")
 
     def __iter__(self):
-        return iter(self.data.items())
+        return iter(self._data.items())
 
     def __bool__(self):
-        return bool(self.data)
+        return bool(self._data)
 
     def get(self, key, default=None):
         keys = key.split('.')
-        value = self.data
+        value = self._data
         for k in keys:
             if isinstance(value, dict) and k in value:
                 value = value[k]
@@ -35,7 +35,7 @@ class YamlObject:
 
     def has(self, key):
         keys = key.split('.')
-        value = self.data
+        value = self._data
         for k in keys:
             if isinstance(value, dict) and k in value:
                 value = value[k]
@@ -44,16 +44,16 @@ class YamlObject:
         return True
 
     def keys(self):
-        return self.data.keys()
+        return self._data.keys()
 
     def values(self):
-        return [YamlObject(value) if isinstance(value, dict) else value for value in self.data.values()]
+        return [YamlObject(value) if isinstance(value, dict) else value for value in self._data.values()]
 
     def items(self):
-        return [(key, YamlObject(value) if isinstance(value, dict) else value) for key, value in self.data.items()]
+        return [(key, YamlObject(value) if isinstance(value, dict) else value) for key, value in self._data.items()]
 
     def __len__(self):
-        return len(self.data)
+        return len(self._data)
 
     def __contains__(self, key):
         return self.has(key)
