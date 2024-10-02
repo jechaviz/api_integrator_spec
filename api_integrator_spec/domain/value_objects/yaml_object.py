57280de8afc55baddb4fbcc869ddbf5952dabcfa
jesus <461913@petco.com>
2024-10-02 08:06:40 -0600

    feat: Implement YAML object parsing for nested dictionaries and lists

--- api_integrator_spec/domain/value_objects/yaml_object.py
+++ api_integrator_spec/domain/value_objects/yaml_object.py
@@ -1,13 +1,15 @@
 class YamlObject:
-    def __init__(self, data):
-        for key, value in data.items():
-            if isinstance(value, dict):
-                setattr(self, key, YamlObject(value))
-            else:
-                setattr(self, key, value)
+  def __init__(self, data):
+    self.data = data
 
-    def __getattr__(self, name):
-        return None
-
-    def __repr__(self):
-        return str(self.__dict__)
+  def __getattr__(self, key):
+    if key in self.data:
+      value = self.data[key]
+      if isinstance(value, dict):
+        return YamlObject(value)
+      elif isinstance(value, list):
+        return [YamlObject(item) if isinstance(item, dict) else item for item in value]
+      else:
+        return value
+    else:
+      raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{key}'")
