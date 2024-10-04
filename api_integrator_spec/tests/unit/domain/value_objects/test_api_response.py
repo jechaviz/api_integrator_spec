6e9f9d2471f6a224bc1478237f86cca47f44919f
jesus <461913@petco.com>
2024-10-03 18:26:54 -0600

    feat: Enhance ApiResponse value object with additional attributes

--- api_integrator_spec/tests/unit/domain/value_objects/test_api_response.py
+++ api_integrator_spec/tests/unit/domain/value_objects/test_api_response.py
@@ -25,11 +25,24 @@ class TestApiResponse:
             self.api_response.nonexistent_attribute
 
     def test_str_method(self):
-        expected_str = f"ApiResponse(status_code=200, body={self.mock_response.text}...)"
+        self.mock_response.headers = {'Content-Type': 'application/json'}
+        self.mock_response.url = 'https://api.example.com'
+        self.mock_response.encoding = 'utf-8'
+        expected_str = f"ApiResponse(status_code=200, headers={{Content-Type=application/json}}, body(json)={{'key': 'value'}}, url=https://api.example.com, encoding=utf-8)"
         assert str(self.api_response) == expected_str
 
     def test_print_api_response(self, capsys):
+        self.mock_response.headers = {'Content-Type': 'application/json'}
+        self.mock_response.url = 'https://api.example.com'
+        self.mock_response.encoding = 'utf-8'
         print(self.api_response)
         captured = capsys.readouterr()
-        expected_output = f"ApiResponse(status_code=200, body={self.mock_response.text}...)\n"
+        expected_output = f"ApiResponse(status_code=200, headers={{Content-Type=application/json}}, body(json)={{'key': 'value'}}, url=https://api.example.com, encoding=utf-8)\n"
         assert captured.out == expected_output
+
+    def test_str_method_with_text_response(self):
+        self.mock_response.text = 'This is a text response'
+        self.mock_response.json.side_effect = ValueError
+        self.api_response = ApiResponse(self.mock_response)
+        expected_str = f"ApiResponse(status_code=200, headers={{}}, body(text)=This is a text response, url=mock://test.com, encoding=utf-8)"
+        assert str(self.api_response) == expected_str
