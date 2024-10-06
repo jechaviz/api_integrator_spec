fef2ca636fe0ef938a33a1fc5d4f9be2c031e4a3
jesus <461913@petco.com>
2024-10-05 18:26:21 -0600

    fix: yml_loader.py

--- src/infrastructure/persistence/yml_loader.py
+++ src/infrastructure/persistence/yml_loader.py
@@ -1,9 +1,9 @@
 import yaml
-from api_integrator_spec.domain.value_objects.yaml_object import YamlObject
+from src.domain.value_objects.yml_obj import YmlObj
 
-class YamlLoader:
+class YmlLoader:
     @staticmethod
     def load(config_path):
         with open(config_path) as f:
             data = yaml.safe_load(f)
-            return YamlObject(data)
+            return YmlObj(data)
