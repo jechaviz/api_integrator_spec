7587fb6d76f8a97c0a95856c266eb1e585dcd25d
jesus <461913@petco.com>
2024-10-05 18:29:18 -0600

    del: several files for simplifcation because the goal is other

--- src/infrastructure/persistence/yml_loader.py
+++
@@ -1,9 +0,0 @@
-import yaml
-from src.domain.value_objects.yml_obj import YmlObj
-
-class YmlLoader:
-    @staticmethod
-    def load(config_path):
-        with open(config_path) as f:
-            data = yaml.safe_load(f)
-            return YmlObj(data)
